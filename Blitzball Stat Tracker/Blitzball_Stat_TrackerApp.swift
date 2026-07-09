95b14125e7f370962ebedafb1d781bc0b5027455
James.Heatherly <James.Heatherly@missionpethealth.com>
2026-07-09 10:53:30 -0400

    Initial Commit

---
+++ Blitzball Stat Tracker/Blitzball_Stat_TrackerApp.swift
@@ -0,0 +1,32 @@
+//
+//  Blitzball_Stat_TrackerApp.swift
+//  Blitzball Stat Tracker
+//
+//  Created by James Heatherly on 7/9/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Blitzball_Stat_TrackerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Blitzball Stat TrackerTests/Blitzball_Stat_TrackerTests.swift
@@ -0,0 +1,18 @@
+//
+//  Blitzball_Stat_TrackerTests.swift
+//  Blitzball Stat TrackerTests
+//
+//  Created by James Heatherly on 7/9/26.
+//
+
+import Testing
+
+struct Blitzball_Stat_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
